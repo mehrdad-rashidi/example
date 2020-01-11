434076db4750e31f6498544c070f453d4c4effb2
mrashidi <m.rashidi@sadad.co.ir>
2020-01-11 10:18:08 +0330

    first commit

---
+++ src/main/java/com/spring/exam/firstspringexam/FirstSpringExamApplication.java
@@ -0,0 +1,13 @@
+package com.spring.exam.firstspringexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/exam/firstspringexam/FirstSpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.exam.firstspringexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
